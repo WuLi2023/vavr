d7cec444b1607c0f49d5773f9a24228244c92c6e
MWorld <mcheng2022@qq.com>
2023-03-27 22:31:22 +0800

    docs: 📝 problem 18 optimized

--- src/test/java/io/vavr/collection/euler/Euler18Test.java
+++ src/test/java/io/vavr/collection/euler/Euler18Test.java
@@ -18,9 +18,12 @@
  */
 package io.vavr.collection.euler;
 
+import io.vavr.Function1;
 import io.vavr.Function3;
 import io.vavr.collection.Vector;
 import org.junit.Test;
+
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -72,6 +75,29 @@ public class Euler18Test {
                     // 否则，函数对象递归地调用自身，并将下一行的相邻两个位置的较大值与当前位置的值相加，返回结果作为当前位置的最大路径和。
                     return value + Math.max(Euler18Test.naive.apply(tr, row + 1, col), Euler18Test.naive.apply(tr, row + 1, col + 1));
                 }
+            }
+    );
+
+    /* 优化方案的时间复杂度为O(n^2)，空间复杂度为O(n^2)。与朴素递归算法相比，优化方案可以更快地计算出最大路径和，并且可以处理更大的输入。 */
+    private final static Function1<Vector<Vector<Integer>>, Integer> optimized = Function1.of(
+            (Vector<Vector<Integer>> tr) -> {
+                int n = tr.size();
+                // 二维数组maxSum来记录从底部到当前位置的最大路径和。
+                int[][] maxSum = new int[n][n];
+                for (int i = 0; i < n; i++) {
+                    // 初始化最后一行的最大路径和，将其设置为对应位置的值。
+                    maxSum[n - 1][i] = tr.get(n - 1).get(i);
+
+                }
+                /* 从倒数第二行开始，逐行计算最大路径和，直到计算出整个三角形的最大路径和。 */
+                for (int i = n - 2; i >= 0; i--) {
+                    for (int j = 0; j <= i; j++) {
+                        // 对于每个位置(i,j)，最大路径和等于该位置的值加上下一行相邻两个位置的最大路径和中的较大值。
+                        maxSum[i][j] = tr.get(i).get(j) + Math.max(maxSum[i + 1][j], maxSum[i + 1][j + 1]);
+                    }
+                }
+                // 最终，最大路径和等于第一行的第一个元素的最大路径和。
+                return maxSum[0][0];
             }
     );
 }
